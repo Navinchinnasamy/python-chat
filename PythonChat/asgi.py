a80bf03388e1c5e0a22ae4dde9a7d95e991d5414
Navin <navinchinnasamy@gmail.com>
2021-07-07 02:12:29 +0530

    Example Polls App with Django Base

---
+++ PythonChat/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for PythonChat project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PythonChat.settings')
+
+application = get_asgi_application()
